29174a13eb44a1d4d736b3beb4c37daa4295715a
AhyanH <hahyan786@gmail.com>
2025-11-13 23:17:59 +0000

    revamped skeleton

--- Elite101.py
+++ Elite101.py
@@ -1,12 +1,19 @@
-options = ["thing1", "thing2", "thing3","thing4", "Exit"]
-print("Welcome User! Let me get to know you:")
+options = ["Best Schools in Round Rock", "Student Support Services", "City Holidays","Educational Resources", "Exit"]
+print("Welcome to the Round Rock Department of Education chatbot! Let me get to know you:")
 print()
 name = input("What is your name? ")
-age = input("How old are you? ")
-print(f"HELLO {name}! {age} is a wonderful age to be.")
+grade = input("What grade is the student you will enquire about today in? ")
+while not isinstance(grade, int):
+    print("That is not a valid grade, sorry.")
+    grade = input("What grade is the student you will enquire about today in? ")
+while grade > 12:
+    print("That is not a valid grade, sorry.")
+    grade = input("What grade is the student you will enquire about today in? ")
+print(f"HELLO {name}!")
 print("How can I help you?")
 for option in options:
     print(f"- {option}")
 choice = input("Type your choice here: ")
 if choice.lower == "exit":
     exit()
+print(lol)
